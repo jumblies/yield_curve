e81237d122727c2239ab6faa9a8c2b0a63dac654
jumblies <glamke@gmail.com>
2021-07-26 15:11:15 -0400

    added shell script for influx post

--- web/fetch_curve.sh
+++ web/fetch_curve.sh
@@ -2,3 +2,6 @@
 #wget "http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month(NEW_DATE)%20eq%203%20and%20year(NEW_DATE)%20eq%202021" --output-document=DailyTreasuryYieldCurveRateData.xml
 wget "https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Datasets/yield.xml" \
 --output-document=/home/glamke/docker/yieldcurve/web/DailyTreasuryYieldCurveRateData.xml
+
+source /home/glamke/docker/yieldcurve/web/parse_and_post_influxdb.sh
+
